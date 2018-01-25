ff45794468b7bc335f978134c530735168f1ffa1
vsusini <31858345+vsusini@users.noreply.github.com>
2018-01-25 11:27:09 -0500

    All Wesbite Files
    Incomplete but progress. Looking to introduce more Javascript into the website. Smoother transitions.

---
+++ Portfolio Website/script.js
@@ -0,0 +1,58 @@
+//alert("This is working!");
+//var ageAsString = prompt("What is your age?", "Write age here!");
+//var age = Number(ageAsString);
+var string = "";
+var animal = "thing";
+
+/*
+//SWITCH STATEMENT
+switch (animal) {
+    case "cat":
+        alert("meow");
+        break;
+    case "dog":
+        alert("woof");
+        break;
+    case "hose":
+        alert("ney bitch");
+        break;
+    default:
+        alert("idk this animal");
+        break;
+}
+*/
+
+//FOR STATEMENT
+/*
+for (var counter = 1; counter < age; counter++){
+    string += "Happy Birthday \n";
+    if (counter % 7 == 0) {
+        break;
+    }
+}
+alert(string);
+*/
+
+//WHILE STATEMENTS
+/*
+while (age > 0) {
+    string += "Happy Birthday \n";
+    // or string = string + "hpd \n"
+    age = age - 1;
+    //or age -= 1;
+}
+alert(string);
+*/
+// or var age = Number(prompt("What is your age? ",""));
+//FOR STATEMENTS
+/*
+if (age < 40) {
+    alert("Your young af");
+} else if (age == 40) {
+    alert("congrats your 40!")
+    
+} else {
+    alert("Dam your old");
+}
+*/
+//alert("Thank You!");
