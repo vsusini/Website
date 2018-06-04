20bd161df4cc54de1141378a06565c9a38666e08
Vince <31858345+vsusini@users.noreply.github.com>
2018-06-04 16:11:37 -0400

    Updating Hamburger Dashboard
    Updated but not complete, still need to fix features that are missing and improve view overall. Links are currently broken and need to be fixed up real quick.

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -1,23 +1,61 @@
 //alert("This is working!");
 //var ageAsString = prompt("What is your age?", "Write age here!");
 //var age = Number(ageAsString);
-$(document).ready(function() {
-    function toggleSidebar() {
-        $(".button").toggleClass("active");
-        $("main").toggleClass("move-to-left");
-        $(".sidebar-item").toggleClass("active");
-    }
 
-    $(".button").on("click tap", function() {
-        toggleSidebar();
-    });
 
-    $(document).keyup(function(e) {
-        if (e.keyCode === 27) {
-        toggleSidebar();
+(function($) {
+
+    $(document).ready(function(){
+      $('.toggle-nav').on('click', function() {
+        toggleNavigation($(this), $('.nav-pane'));
+        changeLetters($(this));
+      });
+  
+      function toggleNavigation(btn, nav) {
+        btn.toggleClass('open');
+        nav.toggleClass('open');
+
+      }
+  
+      function changeLetters(btn) {
+        var m = $('.toggle-nav span.m');
+        var e = $('.toggle-nav span.e');
+        var n = $('.toggle-nav span.n');
+        var u = $('.toggle-nav span.u');
+  
+        e.toggleClass('btn-close');
+  
+        if(btn.hasClass('open'))
+        {
+          m.text("E");
+          m.css("color","white");
+          m.css("top","-0.3vh");
+          n.text("I");
+          n.css("color","white");
+          n.css("top","-0.3vh");
+          u.text("T");
+          u.css("color","white");
+          u.css("top","-0.3vh");
         }
-    });  
-});
+        else
+        {
+          m.text("M");
+          m.css("color","black");
+          m.css("top","0vh");
+          n.text("N");
+          n.css("color","black");
+          n.css("top","0.vh");
+          u.text("U");
+          u.css("color","black");
+          u.css("top","0vh");
+        }
+      }
+    });
+  
+  })(jQuery);
+
+
+
 
 /*
 //SWITCH STATEMENT
