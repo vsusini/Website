25378b81614e4bfc27b44d8e16877ee21c005845
Vince <vmsusini@gmail.com>
2018-06-24 22:11:43 -0400

    Small Cleaning Code Updates
    Just traveled between diffrent files cleaning up code. Also added Ottawa section and removed latin from text.

--- Portfolio Website/script.js
+++ Portfolio Website/script.js
@@ -1,8 +1,3 @@
-//alert("This is working!");
-//var ageAsString = prompt("What is your age?", "Write age here!");
-//var age = Number(ageAsString);
-
-
 (function($) {
 
     $(document).ready(function(){
@@ -53,59 +48,3 @@
     });
 
 })(jQuery);
-
-
-
-
-/*
-//SWITCH STATEMENT
-switch (animal) {
-    case "cat":
-        alert("meow");
-        break;
-    case "dog":
-        alert("woof");
-        break;
-    case "hose":
-        alert("ney bitch");
-        break;
-    default:
-        alert("idk this animal");
-        break;
-}
-*/
-
-//FOR STATEMENT
-/*
-for (var counter = 1; counter < age; counter++){
-    string += "Happy Birthday \n";
-    if (counter % 7 == 0) {
-        break;
-    }
-}
-alert(string);
-*/
-
-//WHILE STATEMENTS
-/*
-while (age > 0) {
-    string += "Happy Birthday \n";
-    // or string = string + "hpd \n"
-    age = age - 1;
-    //or age -= 1;
-}
-alert(string);
-*/
-// or var age = Number(prompt("What is your age? ",""));
-//FOR STATEMENTS
-/*
-if (age < 40) {
-    alert("Your young af");
-} else if (age == 40) {
-    alert("congrats your 40!")
-    
-} else {
-    alert("Dam your old");
-}
-*/
-//alert("Thank You!");
